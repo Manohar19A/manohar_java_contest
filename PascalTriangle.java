c0d1c2141a4b86208951c821f6c29e3af6652831
Manohar19A <manohar.chimata@arshaa.com>
2023-05-20 14:41:17 +0530

    second commit

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,6 +1,16 @@
-public class PascalTriangle{
-    public static void main(String[] args){
-        System.out.println("Hello");
-    }
+public class PascalTriangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j < 5 - i; j++) {
+                System.out.print(" ");
+            }
+            int num = 1;
+            for (int k = 0; k <= i; k++) {
+                System.out.print(num + " ");
+                num = num * (i - k) / (k + 1);
+            }
+            System.out.println();
+        }
 
+    }
 }
