55cf6c5913073fdf799d719b12d1d5bf509ec82a
Manohar19A <manohar.chimata@arshaa.com>
2023-05-20 16:25:05 +0530

    third commit

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,16 +1,47 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class PascalTriangle {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 1; j < 5 - i; j++) {
+  
+
+    public static void printPascalTriangle(int numRows) {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 1; j < numRows - i; j++) {
                 System.out.print(" ");
             }
-            int num = 1;
-            for (int k = 0; k <= i; k++) {
-                System.out.print(num + " ");
-                num = num * (i - k) / (k + 1);
+            for (int j = 0; j <= i; j++) {
+                System.out.print(getPascalValue(i, j) + " ");
             }
             System.out.println();
         }
-
+    }   
+    public static int getPascalValue(int row, int col) {
+        if (col == 0 || col == row) {
+            return 1;
+        } 
+        String key = row + "," + col;
+        if (cache.containsKey(key)) 
+        {
+            return cache.get(key);
+        }
+        else {
+            return getPascalValue(row - 1, col - 1) + getPascalValue(row - 1, col);
+        }
+    }   
+    private static Map<String, Integer> cache = new HashMap<>();
+     public static void main(String[] args) {
+        int numRows = 5;
+        printPascalTriangle(numRows);
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 1; j < 5 - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     int num = 1;
+        //     for (int k = 0; k <= i; k++) {
+        //         System.out.print(num + " ");
+        //         num = num * (i - k) / (k + 1);
+        //     }
+        //     System.out.println();
+        // }
     }
 }
